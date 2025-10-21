090bba23f6073b6b924e2fba2ad6e78c9187eaa6
Dominik <wezik@users.noreply.github.com>
2025-10-22 00:50:28 +0200

    Parse simple template file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{fs::File, io::Read};
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Template {
+    name: String,
+    version: f32,
+    #[serde(rename = "magicNumber")]
+    magic_number: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("template/template.yaml").expect("Failed to open template file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Failed to read template file");
+
+    let template: Template = serde_yml::from_str(&contents).expect("Failed to parse template file");
+    println!("{:?}", template);
 }
